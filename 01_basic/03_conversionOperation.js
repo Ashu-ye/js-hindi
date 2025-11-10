ac3c4b7925883669db28af442e2a2d2aa6648809
Ashu-ye <102645746+Ashu-ye@users.noreply.github.com>
2025-11-10 16:45:22 +0000

    edited

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -16,3 +16,22 @@ console.log(valueInNumber);
 /* 
 *************************** OPERATIONS ************************** 
 */
+let value = 3
+let negative = -value
+console.log(negative)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2)
+console.log(2%2)
+
+let str1 = "hello"
+let str2 = "bro"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2 + 2)
+console.log(1+2 + "2")
+
---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,56 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+
+//  ===
+console.log("2" === 2)
+
+
+// Array , Object , Function
+
+const hero = ["Shaktiman", "Krish", "Nagraj"]
+
+let myObj = {
+    name: "Ashu",
+    age: 22,
+}
+
+const myFunction = function () {
+    console.log("Hello World")
+}
+
+
+
+/*
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+*/
