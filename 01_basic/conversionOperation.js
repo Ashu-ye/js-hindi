78a3b0d7f826bc04c6c01d8ad8f787446d78ccce
Ashu-ye <102645746+Ashu-ye@users.noreply.github.com>
2025-11-10 06:17:48 +0000

    conversion

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,15 @@
+var score = "2345"
+console.log(typeof score);
+console.log(typeof(score));
+var valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+var score = "2345asd"
+console.log(typeof score);
+console.log(typeof(score));
+var valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
