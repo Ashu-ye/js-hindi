df670a8cc7c94b9cb46bfc979a77029857ac042d
Ashu-ye <102645746+Ashu-ye@users.noreply.github.com>
2025-11-10 05:59:26 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all Js cose as newer version it is not compulsory to write it is by default active
+var name2 = "Ashu"
+var number = 24234
+var bollen = true
+var null1 = null
+
+console.table([name2, number, bollen, null1])
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 143424 // Cannot change the value
+let accountEmail = "ashrayyen@gmail.com"
+var accountpassword = "2342"
+accountCity = "Chandrapur"
+
+accountEmail = "ishwar@gmail.com"
+accountpassword = "353"
+accountCity = "Pune"
+
+
+console.table([accountEmail, accountpassword , accountCity])
