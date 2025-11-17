a10768397009e9948d2e04200629c8f3f902bcb5
Ashu-ye <102645746+Ashu-ye@users.noreply.github.com>
2025-11-17 10:08:15 +0000

    objects

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -1,3 +1,4 @@
+
 const tinderUser = {}
 
 tinderUser.is = "123abc"
@@ -25,6 +26,7 @@ console.log(regularUser.fullname.userfullname.firstName);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
+const obj7 = {5: "c", 6: "d"}
 
 const obj3 = {obj1 ,obj2}
 console.log(obj3);
@@ -40,3 +42,48 @@ console.log(obj4); // go to mdn doc to understad more about it
 
 const obj5 = {...obj1, ...obj2} // you always use this type of syntax
 console.log(obj5)
+
+// when objects comes from data base then you access data like following
+
+const user = [ // this is an array seprated object comes form database
+    {
+        id:1,
+        email: "da@WebGLVertexArrayObject.com"
+    },
+    {
+        id:1,
+        email: "da@WebGLVertexArrayObject.com"
+    },
+    {
+        id:1,
+        email: "da@WebGLVertexArrayObject.com"
+    },
+    {
+        id:1,
+        email: "da@WebGLVertexArrayObject.com"
+    }
+]
+
+user[1].email
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // this method tells you if given property exit or not
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+// Destructring of object 
+
+const course = {
+    coursename: "js in practic",
+    price : "999",
+    courseInstructor : "Hitesh"
+}
+
+const {courseInstructor} = course //here you assign  courseInstructor as an variable which you directly use as a variable 
+const {courseInstructor: instructor} = course  // from now you write " instructor" except courseInstructor this is caled Destructring of object
+console.log(instructor);
+
